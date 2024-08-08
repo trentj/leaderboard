65d5e29a03089330765b9be9ab2219a3c59a9afc
Trent Josephsen <trent.josephsen@imec-int.com>
2024-08-08 14:47:11 -0400

    Change workbook format in create_db.py
    + argument parser
    ~ use calamine instead of odfpy
    ~ change format of input workbook
      (no ids in alias tables)

--- create_db.py
+++ create_db.py
@@ -1,25 +1,23 @@
-import datetime
+import argparse
+import pathlib
 import sqlite3
+from itertools import repeat
 
-from odf.opendocument import load
-from odf.table import Table, TableRow
+from python_calamine import CalamineWorkbook
 
 
-def read_simple_table(sheet):
+def read_alias_table(sheet):
     result = {}
-    for row in sheet.getElementsByType(TableRow):
-        if not row.hasChildNodes():
-            break
-        id_, name, *aka = [str(n) for n in row.childNodes if n.hasChildNodes()]
-
-        result[name] = int(id_)
-        for alias in aka:
-            result[alias] = int(id_)
+    id_ = 1
+    for row in sheet.iter_rows():
+        for alias in row:
+            result[alias] = id_
+        id_ += 1
     return result
 
 
 def parse_players(nicknames, players):
-    nicks = str(nicknames).split("+")
+    nicks = nicknames.split("+")
     for nick in nicks:
         if nick in players:
             yield players[nick]
@@ -33,52 +31,59 @@ def parse_players(nicknames, players):
 def read_results_table(sheet, games, players):
     results = []
     first_row = True
-    for row in sheet.getElementsByType(TableRow):
+    for row in sheet.iter_rows():
         result = {}
         if first_row:
             first_row = False
             continue
-        if not row.firstChild.hasChildNodes():
-            break
-        date, game, winner, *others = row.childNodes
-        result["date"] = datetime.datetime.strptime(str(date), "%d %b %Y").date()
-        result["game"] = games[str(game)]
+        date, game, winner, *others = row
+        result["date"] = date
+        result["game"] = games[game]
         result["winner"] = list(parse_players(winner, players))
-        result["others"] = [list(parse_players(p, players)) for p in others if p.hasChildNodes()]
+        result["others"] = [list(parse_players(p, players)) for p in others if p]
         results.append(result)
     return results
 
 
-if __name__ == "__main__":
-    doc = load("GameNightWins.ods")
-    sheets = doc.spreadsheet.getElementsByType(Table)
-    sheets = {sheet.getAttribute("name"): sheet for sheet in sheets}
-    games = read_simple_table(sheets["Games"])
-    players = read_simple_table(sheets["Players"])
-    results = read_results_table(sheets["Results"], games, players)
+def convert_workbook(db: sqlite3.Connection, wb: CalamineWorkbook):
+    games = read_alias_table(wb.get_sheet_by_name("Games"))
+    players = read_alias_table(wb.get_sheet_by_name("Players"))
+    results = read_results_table(wb.get_sheet_by_name("Results"), games, players)
+    cur = db.cursor()
+    cur.executemany("INSERT OR IGNORE INTO game (name, id) VALUES (?, ?);", games.items())
+    cur.executemany("INSERT OR IGNORE INTO player (name, id) VALUES (?, ?);", players.items())
+    db.commit()
+    for result in results:
+        cur.execute("INSERT INTO event (date, game_id) VALUES (?, ?);",
+                    (result["date"].isoformat(), result["game"]))
+        event_id = cur.lastrowid
+        cur.executemany("INSERT INTO result (event_id, player_id, next_teammate, winner) VALUES (?, ?, ?, TRUE)",
+                        zip(repeat(event_id), result["winner"], result["winner"][1:] + [None]))
+        for others in result["others"]:
+            cur.executemany("INSERT INTO result (event_id, player_id, next_teammate, winner) VALUES (?, ?, ?, FALSE)",
+                            zip(repeat(event_id), others, others[1:] + [None]))
+    db.commit()
 
-    db = sqlite3.connect("results.db")
+
+def create_db(db: sqlite3.Connection):
     cur = db.cursor()
     with open("schema.sql") as fp:
         cur.executescript(fp.read())
     db.commit()
 
-    cur.executemany("INSERT OR IGNORE INTO game (name, id) VALUES (?, ?);", games.items())
-    cur.executemany("INSERT INTO player (name, id) VALUES (?, ?);", players.items())
 
-    for result in results:
-        cur.execute("INSERT INTO event (date, game_id) VALUES (?, ?);",
-                    (result["date"].isoformat(), result["game"]))
-        event_id = cur.lastrowid
-        last_player = None
-        for player_id in reversed(result["winner"]):
-            cur.execute("INSERT INTO result (event_id, player_id, next_teammate, winner) VALUES (?, ?, ?, TRUE)",
-                        (event_id, player_id, last_player))
-            last_player = player_id
-        for players in result["others"]:
-            last_player = None
-            for player_id in reversed(players):
-                cur.execute("INSERT INTO result (event_id, player_id, next_teammate, winner) VALUES (?, ?, ?, FALSE)",
-                            (event_id, player_id, last_player))
-                last_player = player_id
-    db.commit()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser("create_db",
+                                     description="Convert a game statistics spreadsheet into a SQLite3 database")
+    parser.add_argument("-c", "--create", nargs='?', const='schema.sql', type=pathlib.Path,
+                        help="Create a new database using the schema file")
+    parser.add_argument("-o", "--out", default='results.db', type=pathlib.Path,
+                        help="Choose the output filename")
+    parser.add_argument("workbook", type=pathlib.Path, help="The workbook (.xlsx or .ods) to be converted")
+    args = parser.parse_args()
+
+    wb = CalamineWorkbook.from_path(args.workbook)
+    db = sqlite3.connect(args.out)
+    if args.create:
+        create_db(db)
+    convert_workbook(db, wb)
